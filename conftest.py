a5cfa5324dd6e2b246af0248c7e8549cf02ee0f3
v.trubnikova <v.trubnikova@mts.ai>
2024-01-31 01:25:19 +0300

    доработаны тесты и добавлена фикстура на очистку строки запроса

--- conftest.py
+++ conftest.py
@@ -9,3 +9,8 @@ def set_browser():
     browser.config.window_height = 720
     yield
     browser.quit()
+
+
+@pytest.fixture()
+def clear_q():
+    browser.element('[name="q"]').clear()
--- test_google_search.py
+++ test_google_search.py
@@ -1,11 +1,13 @@
-from selene import browser, be, have, by
+from selene import browser, be, have
 
 
 def test_selene_can_be_found(set_browser):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
 
-    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+    browser.element('#search').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
-def test_selene_no_results(set_browser):
-    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+def test_selene_no_results(set_browser, clear_q):
+    browser.element('[name="q"]').type('уцоушйцвоовйи').press_enter()
+
+    browser.element('#result-stats').should(have.text('Результатов: примерно 0'))
